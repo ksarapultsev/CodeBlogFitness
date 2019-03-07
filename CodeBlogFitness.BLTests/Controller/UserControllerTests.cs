470e4d2b0fd1b00821f9cb06ed5b5d41278c017d
ksarapultsev <ksarapultsev2014@gmail.com>
2019-03-07 09:41:56 +0600

    Day 2

---
+++ CodeBlogFitness.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBlogFitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            var userName = Guid.NewGuid().ToString();
+            var birthdate = DateTime.Now.AddYears(-18);
+            var weight = 90;
+            var height = 190;
+            var gender = "man";
+            var controller = new UserController(userName);
+            controller.SetNewUserData(gender, birthdate, weight, height);
+            var controller2 = new UserController(userName);
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+
+
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+
+            //Act
+            var controller = new UserController(userName);
+            // Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
